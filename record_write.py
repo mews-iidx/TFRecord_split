006be203dce30ae94fa42736abb4e0605214d9cc
mews <mews@mews-base.com>
2020-01-23 03:56:41 +0000

    fix

--- record_write.py
+++ record_write.py
@@ -5,6 +5,7 @@ import numpy as np
 from tensorflow.train import Feature
 from tensorflow.train import Example
 import io
+import os
 import cv2
 
 
@@ -50,6 +51,20 @@ def pil2cv(image):
     elif new_image.shape[2] == 4:
         new_image = cv2.cvtColor(new_image, cv2.COLOR_RGBA2BGRA)
     return new_image
+def cv2pil(image):
+    new_image = image.copy()
+    if new_image.ndim == 2:
+        pass
+    elif new_image.shape[2] == 3: 
+        new_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB)
+    elif new_image.shape[2] == 4:
+        new_image = cv2.cvtColor(new_image, cv2.COLOR_BGRA2RGBA)
+    new_image = Image.fromarray(new_image)
+    return new_image
+
+def add_num(fname, n):
+    base, prefix  = os.path.splitext(fname)
+    return base + str(n) + prefix
 
 #def _bytes_feature(value):
 #  if isinstance(value, type(tf.constant(0))):
@@ -82,7 +97,10 @@ def img_split(example, cnt_x, cnt_y, offset=100):
     labels = example.features.feature["image/object/class/label"].int64_list.value
     org_height = example.features.feature["image/height"].int64_list.value[0]
     org_width = example.features.feature["image/width"].int64_list.value[0]
+    org_fname = example.features.feature["image/filename"].bytes_list.value[0].decode('utf-8')
     image = example.features.feature["image/encoded"].bytes_list.value[0]
+    fmt = example.features.feature["image/format"].bytes_list.value[0].decode('utf-8')
+
     bs = io.BytesIO(image)
     img_pil = Image.open(bs)
     img = pil2cv(img_pil)
@@ -98,7 +116,6 @@ def img_split(example, cnt_x, cnt_y, offset=100):
     dst_ymaxs = []
     dst_xmins = []
     dst_ymins = []
-    dst_img = []
 
     for x in range(cnt_x):
         for y in range(cnt_y):
@@ -110,13 +127,12 @@ def img_split(example, cnt_x, cnt_y, offset=100):
             org_size = (org_width, org_height)
             x_start, x_stop, y_start, y_stop = calc_offset(x_start, x_stop, y_start, y_stop, org_size, offset)
             sp_img = cp_img.copy()[y_start:y_stop, x_start:x_stop,:]
-            dst_img.append(sp_img)
             for xmax, ymax, xmin, ymin, label in zip(xmaxs, ymaxs, xmins, ymins, labels):
                 org_start_x  = int(xmin * org_width)
                 org_start_y = int(ymin * org_height)
                 org_stop_x = int(xmax * org_width)
                 org_stop_y = int(ymax * org_height)
-                cv2.rectangle(img, (org_start_x, org_start_y), (org_stop_x, org_stop_y), (255,0,0),4)
+                #cv2.rectangle(img, (org_start_x, org_start_y), (org_stop_x, org_stop_y), (255,0,0),4)
 
                 #in the range
                 if (x_start < org_start_x < x_stop) and (y_start < org_start_y < y_stop) and (x_start < org_stop_x < x_stop) and (y_start < org_stop_y < y_stop):
@@ -124,17 +140,40 @@ def img_split(example, cnt_x, cnt_y, offset=100):
                     cur_h = org_height
                     start = ( int(cur_w * xmin) - (x*(x_step - offset)), int(cur_h * ymin) - (y * (y_step - offset)))
                     stop =  ( int(cur_w * xmax) - (x*(x_step - offset)), int(cur_h * ymax) - (y * (y_step - offset)))
-                    cv2.rectangle(sp_img, start, stop,(255, 0, 255), 4)
+                    #cv2.rectangle(sp_img, start, stop,(255, 0, 255), 4)
 
                     dst_xmaxs.append(xmax)
                     dst_ymaxs.append(ymax)
                     dst_xmins.append(xmin)
                     dst_ymins.append(ymin)
                     dst_labels.append(label)
-            fname = 'img_{}_{}.jpg'.format(x,y)
-            cv2.imwrite(fname, sp_img)
-            print("save : " + fname)
-        cv2.imwrite("original.jpg", img)
+            #fname = 'img_{}_{}.jpg'.format(x,y)
+            #cv2.imwrite(fname, sp_img)
+
+
+###WRITE TF RECORD
+            img_str = cv2.imencode('.' + fmt,sp_img)[1].tostring()
+            fname = add_num(org_fname, y + (x*cnt_y))
+            writer = tf.python_io.TFRecordWriter(os.path.splitext(fname)[0] + '.tfrecord')
+            print(fname )
+            height = int(y_stop - y_start)
+            width =  int(x_stop - x_start)
+            example = tf.train.Example(features=tf.train.Features(feature={
+                'image/height': tf.train.Feature(int64_list=tf.train.Int64List(value=[height])),
+                "image/width" : tf.train.Feature(int64_list=tf.train.Int64List(value=[width])),
+                "image/encoded" : tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_str])),
+                "image/filename" : tf.train.Feature(bytes_list=tf.train.BytesList(value=[fname.encode('utf-8')])),
+                "image/object/bbox/xmax" : tf.train.Feature(float_list = tf.train.FloatList(value=dst_xmaxs)),
+                "image/object/bbox/xmin" : tf.train.Feature(float_list = tf.train.FloatList(value=dst_xmins)),
+                "image/object/bbox/ymax" : tf.train.Feature(float_list = tf.train.FloatList(value=dst_ymaxs)),
+                "image/object/bbox/ymin" : tf.train.Feature(float_list = tf.train.FloatList(value=dst_ymins)),
+                "image/object/class/label" : tf.train.Feature(int64_list=tf.train.Int64List(value=dst_labels))
+            }))
+            writer.write(example.SerializeToString())
+        #cv2.imwrite("original.jpg", img)
+
+        #f = open("test.jpg", "wb")
+        #f.write(img_str)
 
 
 if __name__ == '__main__':

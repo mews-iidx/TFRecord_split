bf7456798147a0cd421db85c847b836471ddc95b
y.yamauchi <y.yamauchi@edge-consulting.jp>
2020-02-05 21:21:39 +0900

    fix

--- record_write.py
+++ record_write.py
@@ -1,4 +1,3 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
 from PIL import Image
 import numpy as np
@@ -9,45 +8,15 @@ import os
 import cv2
 import sys
 import glob
-import offset
+import copy
+from offset import split_img
+
+#definitions 
+XCNT = 3
+YCNT = 3
 
 def usage():
     print('Usage: ' + sys.argv[0] + ' <records_dir_path> <output_dir_path>')
-
-
-def calc_offset(x_start, x_stop, y_start, y_stop, org_size, offset):
-    if offset > max(org_size):
-        print("ERROR : requirement max(org_size) > offset")
-        quit(-1)
-    
-    h_offset = offset // 2
-
-    x_start_h = x_start - h_offset
-    x_stop_h = x_stop + h_offset
-    y_start_h = y_start - h_offset
-    y_stop_h = y_stop + h_offset
-
-    x_start_offset = -1 * h_offset
-    x_stop_offset = h_offset
-    y_start_offset = -1 * h_offset
-    y_stop_offset = h_offset
-
-    if x_start_h < 0:
-        x_start_offset +=h_offset
-        x_stop_offset += h_offset 
-    if y_start_h < 0:
-        y_start_offset += h_offset
-        y_stop_offset += h_offset
-
-    if x_stop_h > org_size[0]:
-        x_start_offset += -1 * h_offset
-        x_stop_offset += -1 * h_offset
-    if y_stop_h > org_size[1]:
-        y_start_offset += -1 * h_offset
-        y_stop_offset += -1 * h_offset
-
-
-    return  x_start + x_start_offset, x_stop + x_stop_offset, y_start + y_start_offset, y_stop + y_stop_offset
 
 def pil2cv(image):
     new_image = np.array(image, dtype=np.uint8)
@@ -69,105 +38,121 @@ def cv2pil(image):
     new_image = Image.fromarray(new_image)
     return new_image
 
-def add_num(fname, n):
-    base, prefix  = os.path.splitext(fname)
-    return base + str(n) + prefix
 
+def parse_record(input_file):
+    record_iterator = tf.python_io.tf_record_iterator(input_file)
+    for string_record in record_iterator:
+        example = tf.train.Example()
+        example.ParseFromString(string_record)
+        return example.features.feature
 
-def img_split(example, cnt_x, cnt_y, offset=100, output_path='out'):
-
-#get example values 
-    xmaxs = example.features.feature["image/object/bbox/xmax"].float_list.value
-    ymaxs = example.features.feature["image/object/bbox/ymax"].float_list.value
-    xmins = example.features.feature["image/object/bbox/xmin"].float_list.value
-    ymins = example.features.feature["image/object/bbox/ymin"].float_list.value
-    labels = example.features.feature["image/object/class/label"].int64_list.value
-    org_height = example.features.feature["image/height"].int64_list.value[0]
-    org_width = example.features.feature["image/width"].int64_list.value[0]
-    org_fname = example.features.feature["image/filename"].bytes_list.value[0].decode('utf-8')
-    image = example.features.feature["image/encoded"].bytes_list.value[0]
-    fmt = example.features.feature["image/format"].bytes_list.value[0].decode('utf-8')
-    print(org_width, org_height)
-
+def byte2cv(image):
     bs = io.BytesIO(image)
     img_pil = Image.open(bs)
     img = pil2cv(img_pil)
-    cp_img = img.copy()
-    h = img.shape[0]
-    w = img.shape[1]
+    return img
 
-    x_step = (w // cnt_x) 
-    y_step = (h // cnt_y) 
-    print("step", x_step, y_step)
+def is_november(classname):
+    ng_classes = ['middle_garbage', 'small_garbage']
 
-
-    for x in range(cnt_x):
-        for y in range(cnt_y):
-
-            x_start = (x * x_step) 
-            x_stop  = ((x+1) * x_step)
-            y_start = (y * y_step) 
-            y_stop  = ((y+1) * y_step) 
-            org_size = (org_width, org_height)
-            x_start, x_stop, y_start, y_stop = calc_offset(x_start, x_stop, y_start, y_stop, org_size, offset)
-            sp_img = cp_img.copy()[y_start:y_stop, x_start:x_stop,:]
-            dst_labels = []
-            dst_xmaxs = []
-            dst_ymaxs = []
-            dst_xmins = []
-            dst_ymins = []
-            for xmax, ymax, xmin, ymin, label in zip(xmaxs, ymaxs, xmins, ymins, labels):
-                org_start_x  = int(xmin * org_width)
-                org_start_y = int(ymin * org_height)
-                org_stop_x = int(xmax * org_width)
-                org_stop_y = int(ymax * org_height)
-                #cv2.rectangle(img, (org_start_x, org_start_y), (org_stop_x, org_stop_y), (255,0,0),4)
-
-                #in the range
-                if (x_start < org_start_x < x_stop) and (y_start < org_start_y < y_stop) and (x_start < org_stop_x < x_stop) and (y_start < org_stop_y < y_stop):
-                    x_offset = (x * x_step) - ((x * offset) // 2)
-                    y_offset = (y * y_step) - ((y * offset) // 2)
+    if classname in ng_classes:
+        return True
+    else:
+        return False
+def get_instances(parsed_feature):
+    classnames = parsed_feature['image/object/class/text'].bytes_list.value
+    image      = parsed_feature["image/encoded"].bytes_list.value[0]
+    xmaxs      = parsed_feature["image/object/bbox/xmax"].float_list.value
+    ymaxs      = parsed_feature["image/object/bbox/ymax"].float_list.value
+    xmins      = parsed_feature["image/object/bbox/xmin"].float_list.value
+    ymins      = parsed_feature["image/object/bbox/ymin"].float_list.value
+    labels     = parsed_feature["image/object/class/label"].int64_list.value
+    org_height = parsed_feature["image/height"].int64_list.value[0]
+    org_width  = parsed_feature["image/width"].int64_list.value[0]
+    org_fname  = parsed_feature["image/filename"].bytes_list.value[0].decode('utf-8')
+    fmt        = parsed_feature["image/format"].bytes_list.value[0].decode('utf-8')
+    return classnames, image, xmaxs, ymaxs, xmins, ymins, labels, org_height, org_width, org_fname, fmt
 
 
-                    #print(" original x, y", (int(org_width * xmin), int(org_height * ymin)), (int(org_width * xmax),int(org_height * ymax)))
-                    start = ( org_start_x - x_offset, org_start_y - y_offset)
-                    stop =  ( org_stop_x - x_offset, org_stop_y - y_offset)
-                    #cv2.rectangle(sp_img, start, stop,(255, 0, 255), 4)
+def denorm_point(length, points):
+    for idx, point in  enumerate(points):
+        points[idx] = int(point * length)
 
-                    dst_xmaxs.append(stop[0] / (x_stop - x_start ))
-                    dst_ymaxs.append(stop[1] / (y_stop - y_start))
-                    dst_xmins.append(start[0] / (x_stop - x_start ))
-                    dst_ymins.append(start[1] / (y_stop - y_start))
-                    dst_labels.append(label)
-            #fname = 'img_{}_{}.jpg'.format(x,y)
-            #cv2.imwrite(fname, sp_img)
+def denorm(org_width, org_height, xmaxs, ymaxs, xmins, ymins):
+    denorm_point(org_width, xmaxs)
+    denorm_point(org_height, ymaxs)
+    denorm_point(org_width, xmins)
+    denorm_point(org_height, ymins)
+    return xmaxs, ymaxs, xmins, ymins
+
+def norm_point(length, points):
+    for idx, point in enumerate(points):
+        points[idx] = point / length
+
+def norm(org_width, org_height, xmaxs, ymaxs, xmins, ymins):
+    norm_point(org_width, xmaxs)
+    norm_point(org_height, ymaxs)
+    norm_point(org_width, xmins)
+    norm_point(org_height, ymins)
+    return xmaxs, ymaxs, xmins, ymins
+    
+def point2child_point(xidx, yidx, xstep, ystep, xmaxs, ymaxs, xmins, ymins):
+    for idx, xmax, ymax, xmin, ymin in zip(range(len(xmaxs)), xmaxs, ymaxs, xmins, ymins):
+        xmaxs[idx] = int(xmax - (xidx * xstep))
+        ymaxs[idx] = int(ymax - (yidx * ystep))
+        xmins[idx] = int(xmin - (xidx * xstep))
+        ymins[idx] = int(ymin - (yidx * ystep))
+    return xmaxs, ymaxs, xmins, ymins
+
+def in_range(point, xmaxs, ymaxs, xmins, ymins, labels):
+    dst_xmaxs  = []
+    dst_ymaxs  = []
+    dst_xmins  = []
+    dst_ymins  = []
+    dst_labels = []
+    for idx, xmax, ymax, xmin, ymin, label in zip(range(len(xmaxs)), xmaxs, ymaxs, xmins, ymins, labels):
+        if not point[0] < xmax < point[2]:
+            continue
+        if not point[1] < ymax < point[3]:
+            continue
+        if not point[0] < xmin < point[2]:
+            continue
+        if not point[1] < ymin < point[3]:
+            continue
+        dst_xmaxs.append(xmax)
+        dst_ymaxs.append(ymax)
+        dst_xmins.append(xmin)
+        dst_ymins.append(ymin)
+        dst_labels.append(label)
+    return dst_xmaxs, dst_ymaxs, dst_xmins, dst_ymins, dst_labels
+
+def img_encode(fmt, img):
+    img_str = cv2.imencode('.' + fmt, img)[1].tostring()
+    return img_str
+
+def put_example(height, width, img, fname, fmt, dst_xmaxs, dst_ymaxs, dst_xmins, dst_ymins, dst_labels):
+    img_str = img_encode(fmt, img)
+    example = tf.train.Example(features=tf.train.Features(feature={
+        'image/height': tf.train.Feature(int64_list=tf.train.Int64List(value=[height])),
+        "image/width" : tf.train.Feature(int64_list=tf.train.Int64List(value=[width])),
+        "image/encoded" : tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_str])),
+        "image/filename" : tf.train.Feature(bytes_list=tf.train.BytesList(value=[fname.encode('utf-8')])),
+        "image/object/bbox/xmax" : tf.train.Feature(float_list = tf.train.FloatList(value=dst_xmaxs)),
+        "image/object/bbox/xmin" : tf.train.Feature(float_list = tf.train.FloatList(value=dst_xmins)),
+        "image/object/bbox/ymax" : tf.train.Feature(float_list = tf.train.FloatList(value=dst_ymaxs)),
+        "image/object/bbox/ymin" : tf.train.Feature(float_list = tf.train.FloatList(value=dst_ymins)),
+        "image/object/class/label" : tf.train.Feature(int64_list=tf.train.Int64List(value=dst_labels))
+    }))
+    return example
 
 
 
-###WRITE TF RECORD
-            img_str = cv2.imencode('.' + fmt,sp_img)[1].tostring()
-            fname = add_num(org_fname, y + (x*cnt_y))
-            writer = tf.python_io.TFRecordWriter(os.path.join(output_path, os.path.splitext(fname)[0] + '.tfrecord'))
-            width =  (org_width //  cnt_x) + offset
-            height =  (org_height //  cnt_y) + offset
-            print(width, height)
-            print(fname , width, height, sp_img.shape)
-            example = tf.train.Example(features=tf.train.Features(feature={
-                'image/height': tf.train.Feature(int64_list=tf.train.Int64List(value=[height])),
-                "image/width" : tf.train.Feature(int64_list=tf.train.Int64List(value=[width])),
-                "image/encoded" : tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_str])),
-                "image/filename" : tf.train.Feature(bytes_list=tf.train.BytesList(value=[fname.encode('utf-8')])),
-                "image/object/bbox/xmax" : tf.train.Feature(float_list = tf.train.FloatList(value=dst_xmaxs)),
-                "image/object/bbox/xmin" : tf.train.Feature(float_list = tf.train.FloatList(value=dst_xmins)),
-                "image/object/bbox/ymax" : tf.train.Feature(float_list = tf.train.FloatList(value=dst_ymaxs)),
-                "image/object/bbox/ymin" : tf.train.Feature(float_list = tf.train.FloatList(value=dst_ymins)),
-                "image/object/class/label" : tf.train.Feature(int65_list=tf.train.Int64List(value=dst_labels))
-            }))
-            writer.write(example.SerializeToString())
-        #cv2.imwrite("original.jpg", img)
 
-        #f = open("test.jpg", "wb")
-        #f.write(img_str)
+def get_imgidx(idx, xcnt, ycnt):
+    y = idx // xcnt
+    x = idx % xcnt
+
+    return x, y
 
 
 if __name__ == '__main__':
@@ -175,7 +160,6 @@ if __name__ == '__main__':
     if argc < 3:
         usage()
         quit()
-    
     input_path = sys.argv[1]
     output_path = sys.argv[2]
 
@@ -185,10 +169,54 @@ if __name__ == '__main__':
     input_files = glob.glob(os.path.join(input_path, '*.tfrecord'))
 
     for input_file in input_files:
-        record_iterator = tf.python_io.tf_record_iterator(input_file)
+        try:
+            parsed_feature = parse_record(input_file) # parse and return
+            #TODO: delete line of the below 
+            #org_feature = parse_record(input_file)
+
+            #get values
+            classnames, bimage, xmaxs, ymaxs, xmins, ymins, labels, org_height, org_width, org_fname, fmt = get_instances(parsed_feature)
+        except:
+            print("skipping Error file : " + input_file, file=sys.stderr)
+            continue
         
-        for string_record in record_iterator:
-            example = tf.train.Example()
-            example.ParseFromString(string_record)
-            #img_split(example, 3, 3)
-            img_split(example, 3, 3, offset=100, output_path=output_path)
+        #ignore cases
+        ## is no object 
+        if len(classnames) == 0 :
+            continue
+        ## is November data
+        decode_name = classnames[0].decode('utf-8')
+        if is_november(decode_name):
+            print(" skipping class : " + decode_name)
+            continue
+
+        #trans norm points to denorm points
+        xmaxs, ymaxs, xmins, ymins = denorm(org_width, org_height, xmaxs, ymaxs, xmins, ymins)
+
+        #trans bytes to opencv img
+        img = byte2cv(bimage)
+
+        #split image xcnt x ycnt
+        xcnt = XCNT
+        ycnt = YCNT
+        sp_imgs, points = split_img(img, xcnt, ycnt, offset_size=0)
+
+        #split images processing
+        for idx, img in enumerate(sp_imgs):
+            ystep, xstep, _ = img.shape
+            dst_xmaxs, dst_ymaxs, dst_xmins, dst_ymins, dst_labels = in_range(points[idx],xmaxs, ymaxs, xmins, ymins, labels)
+            if len(dst_xmaxs) ==0:
+                continue
+
+            x, y = get_imgidx(idx, xcnt, ycnt)
+            dst_xmaxs, dst_ymaxs, dst_xmins, dst_ymins = point2child_point(x, y, xstep, ystep, dst_xmaxs, dst_ymaxs, dst_xmins, dst_ymins)
+            dst_xmaxs, dst_ymaxs, dst_xmins, dst_ymins = norm(xstep, ystep, dst_xmaxs, dst_ymaxs, dst_xmins, dst_ymins)
+
+            #write record
+            record_name = os.path.join(output_path, os.path.splitext(os.path.basename(input_file))[0] + str(idx)  + '.tfrecord')
+            writer = tf.python_io.TFRecordWriter(record_name)
+            example = put_example(ystep, xstep, img, org_fname, fmt, dst_xmaxs, dst_ymaxs, dst_xmins, dst_ymins, dst_labels)
+            writer.write(example.SerializeToString())
+            print(' saved : ' + record_name)
+            print("DEBUG QUIT")
+            quit()
